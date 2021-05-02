e8e2da20b6b463808e1fb497eeac3eff0bbee1af
satorhho <lbriones@gbox.adnu.edu.ph>
2021-05-02 22:07:32 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verbose_user/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verbose_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/LoginPage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              "User App",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
